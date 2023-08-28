aefc8fe87b7279e849fa294c0d2b5fba7db28fba
Kathryn6368 <6368@holbertonstudents.com>
2023-08-27 19:52:19 -0700

    change Squence to List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ This module contains a simple function """
-from typing import Sequence
+from typing import List
 
 
-def sum_list(input_list: Sequence[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ A function to return sum of list of floats """
     sum: float = 0
     for i in input_list:
