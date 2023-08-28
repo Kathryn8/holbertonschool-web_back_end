98d6eeb17bced8b7f4b8ee333f4ac604e590cf7c
Kathryn6368 <6368@holbertonstudents.com>
2023-08-28 16:30:05 -0700

    initial commit

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A module that contains a simple function """
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> int:
+    """ Waits a random amount of time """
+    seconds_to_wait = random.uniform(0, max_delay)
+    await asyncio.sleep(seconds_to_wait)
+    return seconds_to_wait
