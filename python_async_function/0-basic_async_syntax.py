b7f477d0c8f44f61d365153a4bcae8fdf59b060d
Kathryn6368 <6368@holbertonstudents.com>
2023-08-28 16:34:27 -0700

    correct type of return value

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@
 import asyncio
 import random
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """ Waits a random amount of time """
     seconds_to_wait: float = random.uniform(0, max_delay)
     await asyncio.sleep(seconds_to_wait)
