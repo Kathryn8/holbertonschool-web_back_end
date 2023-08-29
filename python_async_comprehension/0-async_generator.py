83ae7f5a08b4ea578c3e275f793310d650d13cb2
Kathryn6368 <6368@holbertonstudents.com>
2023-08-28 21:40:24 -0700

    fix return value type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ A module that contains a simple function """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+
+async def async_generator() -> Generator[float, None, None]:
     """ an asynchronous generator that yields values """
     for i in range(10):
         await asyncio.sleep(1)
