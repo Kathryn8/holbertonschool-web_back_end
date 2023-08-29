47e8d6f29b2b879c9b56797590335463160a803d
Kathryn6368 <6368@holbertonstudents.com>
2023-08-28 21:37:42 -0700

    add file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,9 @@
 """ A module that contains a simple function """
 import asyncio
 import random
-import typing
+from typing import AsyncGenerator
 
-
-async def async_generator() -> typing.Generator[float, NoneType, NoneType]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """ an asynchronous generator that yields values """
     for i in range(10):
         await asyncio.sleep(1)
