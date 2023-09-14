e3fb4453780541c331508823c126a1e16651eabc
Kathryn6368 <6368@holbertonstudents.com>
2023-09-14 10:44:21 +1000

    add/update file

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (let idx of array) {
+    const value = idx;
+    idx = appendString + value;
+    newArray.push(idx);
+  }
+  return newArray;
+}
