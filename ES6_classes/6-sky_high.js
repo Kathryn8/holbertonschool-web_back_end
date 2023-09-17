06c8466fc24e15d304756d578f0232f6428c3e68
Kathryn6368 <6368@holbertonstudents.com>
2023-09-17 13:35:40 +1000

    add/update file

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get sqft() {
+    /* eslint no-underscore-dangle: ["error", { "allow": ["_sqft", "_floors"] }] */
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
