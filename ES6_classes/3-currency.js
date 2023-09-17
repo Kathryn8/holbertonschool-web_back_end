fac481d2ca677b789e919cdc4f5ba102078df492
Kathryn6368 <6368@holbertonstudents.com>
2023-09-17 12:26:29 +1000

    add/update file

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  displayFullCurrency() {
+    /* eslint no-underscore-dangle: ["error", { "allow": ["_code", "_name"] }] */
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+}
