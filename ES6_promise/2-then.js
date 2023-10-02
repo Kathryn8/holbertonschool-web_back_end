acfddcee5a61392f363eb86e7868c9bda8898506
Kathryn6368 <6368@holbertonstudents.com>
2023-10-02 16:36:50 +1100

    add/update file

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,18 +1,16 @@
 function handleResponseFromAPI(promise) {
-  function success() {
-    return { status: 200, body: 'Success' };
-  }
-
-  function failure() {
-    return new Error();
-  }
-
-  function log() {
-    console.log('Got a response from the API');
-  }
   promise
-    .then(success, failure)
-    .finally(log);
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
 
 export default handleResponseFromAPI;
