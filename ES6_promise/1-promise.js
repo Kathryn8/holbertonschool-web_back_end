94676b1f293fcb1855f84129adfbb90e180738cf
Kathryn6368 <6368@holbertonstudents.com>
2023-10-02 13:30:11 +1100

    add/update file

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  let promiseA = 0;
+  if (success === true) {
+    promiseA = new Promise((resolve) => {
+      resolve({ status: 200, body: 'Success' });
+    });
+  } else {
+    promiseA = new Promise((reject) => {
+      reject(Error('The fake API is not working currently'));
+    });
+  }
+  return promiseA;
+}
