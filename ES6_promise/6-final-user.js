a454c712d0e84c3804dac2fd80b7c804028bb1a8
Kathryn6368 <6368@holbertonstudents.com>
2023-10-03 08:06:28 +1100

    add/update file

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,22 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promiseA = uploadPhoto(fileName);
-  const promiseB = signUpUser(firstName, lastName);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
 
-  return Promise.allSettled([promiseA, promiseB])
-    .then((data) => {
-      console.log(data);
-    })
-    .catch(() => { })
-    .finally(([user, photo]) => [
-      {
-        status: user.status,
-        value: user.status === 'fulfilled' ? user.value : user.reason,
-      },
-      {
-        status: photo.status,
-        value: photo.status === 'fulfilled' ? photo.value : photo.reason,
-      }]);
+  const filePromise = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([userPromise, filePromise]);
 }
