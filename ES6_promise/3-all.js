c295de0d532029dfc459b60a306919bbcb09677a
Kathryn6368 <6368@holbertonstudents.com>
2023-10-02 18:33:03 +1100

    add/update file

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,27 @@
-import { uploadPhoto, createUser } from './utils';
+
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+  import('./utils.js')
+    .then((utils) => {
+      Promise.all([utils.uploadPhoto(), utils.createUser()])
     })
-    .catch(() => {
-      console.error('Signup system offline');
+    .then((value) => {
+      //console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+      console.log(value)
+    })
+    .catch((e) => {
+      console.log('Signup system offline', e);
     });
 }
+
+// import { uploadPhoto, createUser } from './utils';
+
+// export default function handleProfileSignup() {
+//   return Promise.all([uploadPhoto(), createUser()])
+//     .then(([photoResponse, userResponse]) => {
+//       console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+//     })
+//     .catch(() => {
+//       console.log('Signup system offline');
+//     });
+// }
