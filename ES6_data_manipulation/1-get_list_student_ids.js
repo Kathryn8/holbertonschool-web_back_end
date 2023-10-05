7aa9ebc4435db225ba8e565019781055a9e9c9d4
Kathryn6368 <6368@holbertonstudents.com>
2023-10-05 11:40:04 +1100

    add/update file

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(studentList) {
+  let returnArray = [];
+  if (Array.isArray(studentList) === true) {
+    returnArray = studentList.map((x) => x.id)
+  }
+
+  return returnArray;
+}
