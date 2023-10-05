49d3a9b15f025dbffe3ac274ad9aa1680057899c
Kathryn6368 <6368@holbertonstudents.com>
2023-10-06 08:49:54 +1100

    add/update file

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const shopping = new Map();
+  shopping.set('Apples', 10);
+  shopping.set('Tomatoes', 10);
+  shopping.set('Pasta', 1);
+  shopping.set('Rice', 1);
+  shopping.set('Banana', 5);
+
+  return shopping;
+}
