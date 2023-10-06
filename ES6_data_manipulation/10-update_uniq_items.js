17953cfec4ef4da06644074225b603372e954cd8
Kathryn6368 <6368@holbertonstudents.com>
2023-10-06 15:34:12 +1100

    add/update file

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   map.forEach((value, key, myMap) => {
     if (value === 1) {
       myMap.set(key, 100);
